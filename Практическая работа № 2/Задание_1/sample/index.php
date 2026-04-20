99ed4eb715a1e5690d3d99e28197766bedd22487
arbuzovoleg <danya0112200777@gmail.com>
2026-04-20 14:34:03 +0600

    123

---
+++ Практическая работа № 2/Задание_1/sample/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+
+
+	
+</body>
+</html>	
+
+<?php
+	echo '<h1>Основы rgtпрограммирования</h1>';
+	print("<>Вh2ведение в PHP</h2>");
+	print('<br><hru>jyjyj</hru><br>');
+	var_dump('<h2>Начинаем программировать в среде PHP</h2>');
+
+
+?>
+sos me pleas
